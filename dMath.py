8dc8eee6647aa5e291d2a59ec8e0f278303cc64f
DNVIC <37513362+DNVIC@users.noreply.github.com>
2019-04-15 11:39:47 -0400

    Create dMath.py

---
+++ dMath.py
@@ -0,0 +1,11 @@
+def sRoot(var):
+  return var ** 0.5
+def cRoot(var):
+  return var ** 1/3
+def square(var):
+  return var ** 2
+def cube(var):
+  return var ** 3
+def logarithm(num, base):
+  temp = base ** num
+  return base ** temp
